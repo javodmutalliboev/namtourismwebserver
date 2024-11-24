44d54710a34124733a78937e21928b2fc5fe4b32
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-11-24 16:15:13 +0500

    some progress

--- client/models.py
+++ client/models.py
@@ -1,20 +1,15 @@
 import os
-import shutil
 from django.db import models
-from django.db.models.signals import post_delete, pre_delete
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from datetime import datetime
 
 
 def news_image_upload_path(instance, filename):
-    # Use the instance's ID to construct the upload path for NewsImage
-    return f"news/{instance.news.id}/images/{filename}"
-
-
-def news_main_image_upload_path(instance, filename):
-    # Use the instance's ID to construct the upload path for the main image in News
-    if instance.id:
-        return f"news/{instance.id}/image/{filename}"
-    return f"news/temp/{filename}"
+    # Generate the filename with the current time including milliseconds
+    current_time = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    filename = f"{current_time}_{filename}"
+    return os.path.join("news", filename)
 
 
 class News(models.Model):
@@ -22,7 +17,7 @@ class News(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to=news_main_image_upload_path)
+    image = models.ImageField(upload_to=news_image_upload_path)
     video_i_frame = models.TextField(blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
 
@@ -33,27 +28,13 @@ class News(models.Model):
         verbose_name = "Yangilik"
         verbose_name_plural = "Yangiliklar"
 
-    def save(self, *args, **kwargs):
-        # Save the instance first to ensure it has an ID
-        super().save(*args, **kwargs)
-        if "image" in self.__dict__:
-            image_path = news_main_image_upload_path(self, self.image.name)
-            if self.image.path != image_path:
-                os.makedirs(os.path.dirname(image_path), exist_ok=True)
-                shutil.move(self.image.path, image_path)
-                self.image.name = image_path
-                super().save(update_fields=["image"])
 
-    def delete(self, *args, **kwargs):
-        # Delete the main image file
-        if self.image:
-            if os.path.isfile(self.image.path):
-                os.remove(self.image.path)
-        # Delete the directory containing the images
-        image_dir = os.path.join("news", str(self.id))
-        if os.path.isdir(image_dir):
-            shutil.rmtree(image_dir)
-        super().delete(*args, **kwargs)
+@receiver(post_delete, sender=News)
+def delete_news_image_file(sender, instance, **kwargs):
+    # Delete the image file
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
 
 
 class NewsImage(models.Model):
@@ -70,17 +51,10 @@ class NewsImage(models.Model):
 
 @receiver(post_delete, sender=NewsImage)
 def delete_news_image_file(sender, instance, **kwargs):
-    # Delete the image file
+    # Delete the image file when the news image object is deleted
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
-
-
-@receiver(pre_delete, sender=News)
-def delete_news_images(sender, instance, **kwargs):
-    # Delete all related NewsImage instances
-    for news_image in instance.images.all():
-        news_image.delete()
 
 
 class Holiday(models.Model):
