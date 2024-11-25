b8c18fb1c8619373b6be4b62a810e5ee81efa03a
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-11-25 19:15:35 +0500

    some progress

--- client/serializers.py
+++ client/serializers.py
@@ -2,13 +2,31 @@ from rest_framework import serializers
 from .models import News, NewsImage
 
 
+class NewsImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsImage
+        fields = ["image"]
+
+
 class NewsSerializer(serializers.ModelSerializer):
     slug = serializers.SerializerMethodField()
     image = serializers.SerializerMethodField()
+    images = NewsImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = News
-        fields = "__all__"
+        fields = [
+            "id",
+            "title",
+            "content",
+            "created_at",
+            "updated_at",
+            "image",
+            "video_i_frame",
+            "location",
+            "slug",
+            "images",
+        ]
 
     def get_slug(self, obj):
         return obj.title.lower().replace(" ", "-")
@@ -17,9 +35,3 @@ class NewsSerializer(serializers.ModelSerializer):
         if obj.image:
             return obj.image.url
         return None
-
-
-class NewsImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = NewsImage
-        fields = ["image"]
