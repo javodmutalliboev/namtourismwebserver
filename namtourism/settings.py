d6ed01e60d3e1b453d0f0f64b341fd57910408d7
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-12-27 12:10:04 +0500

    Update settings.py

--- namtourism/settings.py
+++ namtourism/settings.py
@@ -38,6 +38,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://namturapi.fazo-software.uz",
+    "https://namtur.fazo-software.uz"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://namturapi.fazo-software.uz",
